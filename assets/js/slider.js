442cb538620846b1380b704a4bb673277c171999
Adeilson Silva <adeilson.silva@agenciagenia.com.br>
2022-06-09 23:44:17 -0300

    destination page finished

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,6 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
     let i;
 
@@ -9,18 +13,22 @@ function showSlides(n) {
     let dots = document.getElementsByClassName('counter');
 
     if(n > slides.length) {
-        slideIndex - 1;
+        slideIndex = 1;
     }
 
     if(n < 1) {
         slideIndex = slides.length;
+    }
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
 
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
 
-    slides[slideIndex - 1].style.display = "block";
+    slides[slideIndex - 1].style.display = "flex";
 
     dots[slideIndex -1].className += " active";
 }
